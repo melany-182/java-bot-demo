d3227be81bbf3b6eb879728ba8e8051070b5046c
melany-182 <melakaune@gmail.com>
2021-09-21 15:32:36 -0400

    Funcional para varios usuarios simultáneos

--- src/main/java/bo/edu/ucb/est/Calculadora.java
+++ src/main/java/bo/edu/ucb/est/Calculadora.java
@@ -1,12 +1,13 @@
 package bo.edu.ucb.est;
+import java.util.ArrayList;
+
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Calculadora extends TelegramLongPollingBot {
-	public int flag=0; // variable de instancia
-	public int num1=0,num2,suma;
+	public ArrayList<Usuario> usuarios=new ArrayList<Usuario>();
 	
     @Override
     public String getBotToken() {
@@ -15,71 +16,95 @@ public class Calculadora extends TelegramLongPollingBot {
     
     @Override
     public void onUpdateReceived(Update update) { // se ejecuta cada vez que llega un mensaje
-    	System.out.println(flag+"\n");
+    	
+    	for (int i=0; i<usuarios.size(); i++) {
+    		System.out.println(usuarios.get(i).toString());
+    	}
+    	
+    	int aux=0;
+    	Long idusuario=update.getMessage().getFrom().getId();
+    	for (int i=0; i<usuarios.size(); i++) { // verificación de existencia
+    		if ((usuarios.get(i).getUserId()).equals(idusuario)) { // si ya existe
+    			aux=1;
+    			break;
+    		}
+    	}
+    	if (aux==0) { // si no existe todavía
+    		Usuario usuario=new Usuario(idusuario,0);
+        	usuarios.add(usuario);
+    	}
+    
     	System.out.println("Mensaje: "+update.toString()+"\n");
+    	
         if (update.hasMessage()) {
-        	if (flag==0) {
-                enviarMensaje(update,"Bienvenido al Bot Calculadora.\nSeleccione una de las siguientes opciones:\n1. Sumar dos números.\n2. Calcular serie de Fibonacci.");
-                flag=1;
-                return; // importante ********************
-        	}
-        	if (flag==1) { // se le envió el mensaje inicial
-        		try {
-        			int opcion=Integer.parseInt(update.getMessage().getText());
-                	if (opcion==1) {
-                		enviarMensaje(update,"Ingrese el primer número:");
-                        flag=2;
-                        return;
-                	}
-                	else if (opcion==2) {
-                		enviarMensaje(update,"Funcionalidad no implementada. Intente otro día.");
-                        flag=0;
-                        enviarMensaje(update,"Bienvenido al Bot Calculadora.\nSeleccione una de las siguientes opciones:\n1. Sumar dos números.\n2. Calcular serie de Fibonacci.");
-                        flag=1;
-                        return;
-                	}
-                	else {
-                		enviarMensaje(update,"Bienvenido al Bot Calculadora.\nSeleccione una de las siguientes opciones:\n1. Sumar dos números.\n2. Calcular serie de Fibonacci.");
-                        flag=1;
-                        return;
-                	}
-        		}
-        		catch (NumberFormatException e) {
-        			e.printStackTrace();
-        			enviarMensaje(update,"Bienvenido al Bot Calculadora.\nSeleccione una de las siguientes opciones:\n1. Sumar dos números.\n2. Calcular serie de Fibonacci.");
-                    flag=1;
-        			return;
-        		}
-        	}
-        	if (flag==2) {
-        		try {
-        			num1=Integer.parseInt(update.getMessage().getText());
-        			enviarMensaje(update,"Ingrese el segundo número:");
-                    flag=3;
-                    return;
-        		}
-        		catch (NumberFormatException e) {
-        			e.printStackTrace();
-        			enviarMensaje(update,"Bienvenido al Bot Calculadora.\nSeleccione una de las siguientes opciones:\n1. Sumar dos números.\n2. Calcular serie de Fibonacci.");
-                    flag=1;
-                    return;
-        		}
-        	}
-        	if (flag==3) {
-        		try {
-        			num2=Integer.parseInt(update.getMessage().getText());
-            		suma=num1+num2;
-            		enviarMensaje(update,"La suma de los números es: "+suma+".");
-                    flag=0;
-                    enviarMensaje(update,"Bienvenido al Bot Calculadora.\nSeleccione una de las siguientes opciones:\n1. Sumar dos números.\n2. Calcular serie de Fibonacci.");
-                    flag=1;
-                    return;
-        		}
-        		catch (NumberFormatException e) {
-        			e.printStackTrace();
-        			enviarMensaje(update,"Bienvenido al Bot Calculadora.\nSeleccione una de las siguientes opciones:\n1. Sumar dos números.\n2. Calcular serie de Fibonacci.");
-                    flag=1;
-                    return;
+        	for (int i=0; i<usuarios.size(); i++) {
+        		if ((usuarios.get(i).getUserId()).equals(update.getMessage().getFrom().getId())) {
+        			if ((usuarios.get(i).getFlag())==0) {
+        				enviarMensaje(update,"Bienvenido al Bot Calculadora.\nSeleccione una de las siguientes opciones:\n1. Sumar dos números.\n2. Calcular serie de Fibonacci.");
+                        usuarios.get(i).setFlag(1);
+                        return; // importante ********************
+        			}
+        			if ((usuarios.get(i).getFlag())==1) { // se le envió el mensaje inicial
+        				try {
+                			int opcion=Integer.parseInt(update.getMessage().getText());
+                			usuarios.get(i).setOpcion(opcion);
+                        	if ((usuarios.get(i).getOpcion())==1) {
+                        		enviarMensaje(update,"Ingrese el primer número:");
+                                usuarios.get(i).setFlag(2);
+                                return;
+                        	}
+                        	else if ((usuarios.get(i).getOpcion())==2) {
+                        		enviarMensaje(update,"Funcionalidad no implementada. Intente otro día.");
+                                usuarios.get(i).setFlag(0);
+                                enviarMensaje(update,"Bienvenido al Bot Calculadora.\nSeleccione una de las siguientes opciones:\n1. Sumar dos números.\n2. Calcular serie de Fibonacci.");
+                                usuarios.get(i).setFlag(1);
+                                return;
+                        	}
+                        	else {
+                        		enviarMensaje(update,"Bienvenido al Bot Calculadora.\nSeleccione una de las siguientes opciones:\n1. Sumar dos números.\n2. Calcular serie de Fibonacci.");
+                                usuarios.get(i).setFlag(1);
+                                return;
+                        	}
+                		}
+                		catch (NumberFormatException e) {
+                			e.printStackTrace();
+                			enviarMensaje(update,"Bienvenido al Bot Calculadora.\nSeleccione una de las siguientes opciones:\n1. Sumar dos números.\n2. Calcular serie de Fibonacci.");
+                			usuarios.get(i).setFlag(1);
+                			return;
+                		}
+        			}
+        			if ((usuarios.get(i).getFlag())==2) {
+        				try {
+                			int num1=Integer.parseInt(update.getMessage().getText());
+                			usuarios.get(i).setNum1(num1);
+                			enviarMensaje(update,"Ingrese el segundo número:");
+                			usuarios.get(i).setFlag(3);
+                            return;
+                		}
+                		catch (NumberFormatException e) {
+                			e.printStackTrace();
+                			enviarMensaje(update,"Bienvenido al Bot Calculadora.\nSeleccione una de las siguientes opciones:\n1. Sumar dos números.\n2. Calcular serie de Fibonacci.");
+                			usuarios.get(i).setFlag(1);
+                            return;
+                		}
+        			}
+        			if ((usuarios.get(i).getFlag())==3) {
+        				try {
+                			int num2=Integer.parseInt(update.getMessage().getText());
+                			usuarios.get(i).setNum2(num2);
+                    		enviarMensaje(update,"La suma de los números es: "+((usuarios.get(i).getNum1())+(usuarios.get(i).getNum2()))+".");
+                    		usuarios.get(i).setFlag(0);
+                            enviarMensaje(update,"Bienvenido al Bot Calculadora.\nSeleccione una de las siguientes opciones:\n1. Sumar dos números.\n2. Calcular serie de Fibonacci.");
+                            usuarios.get(i).setFlag(1);
+                            return;
+                		}
+                		catch (NumberFormatException e) {
+                			e.printStackTrace();
+                			enviarMensaje(update,"Bienvenido al Bot Calculadora.\nSeleccione una de las siguientes opciones:\n1. Sumar dos números.\n2. Calcular serie de Fibonacci.");
+                			usuarios.get(i).setFlag(1);
+                            return;
+                		}
+        			}
         		}
         	}
         }
---
+++ src/main/java/bo/edu/ucb/est/Usuario.java
@@ -0,0 +1,60 @@
+package bo.edu.ucb.est;
+
+public class Usuario {
+	public Long userId;
+	public int flag;
+	private int opcion;
+	private int num1;
+	private int num2;
+	
+	public Usuario(Long userId,int flag) {
+		this.userId=userId;
+		this.flag=flag;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public int getFlag() {
+		return flag;
+	}
+
+	public int getOpcion() {
+		return opcion;
+	}
+
+	public int getNum1() {
+		return num1;
+	}
+
+	public int getNum2() {
+		return num2;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId=userId;
+	}
+	
+	public void setFlag(int flag) {
+		this.flag=flag;
+	}
+	
+	public void setOpcion(int opcion) {
+		this.opcion=opcion;
+	}
+	
+	public void setNum1(int num1) {
+		this.num1=num1;
+	}
+	
+	public void setNum2(int num2) {
+		this.num2=num2;
+	}
+	
+	@Override
+	public String toString() {
+		return "Usuario [userId=" + userId + ", flag=" + flag + ", opcion=" + opcion + ", num1=" + num1 + ", num2="
+				+ num2 + "]\n";
+	}
+}
